1bec3737f7d9e545152df14ae86b9887bae62748
peterkavuma04-boop <peterkavuma04@gmail.com>
2026-03-18 00:50:00 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "wal Bot",
+  botName: "kight Bot",
   botOwner: 'WalkerPeter', // Your name
   ownerNumber: '256787920740', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
